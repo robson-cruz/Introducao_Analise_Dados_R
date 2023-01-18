2c3a5f8c0b2dc1e0898eb082cf1afdd13413b5e8
Robson Cruz Santos <rcflorestal@yahoo.com.br>
2023-01-18 11:37:24 -0300

    script dados aula 3

---
+++ dados_modulo_1_aula_3.R
@@ -0,0 +1,13 @@
+# Carregar o dataframe
+df <- Sleuth3::case0902
+
+# definir vetores
+especies <- as.vector(df$Species)
+cerebro <- df$Brain
+corpo <- df$Body
+prole <- df$Litter
+gestacao <- df$Gestation
+
+# Salvar os vetores
+save(especies, cerebro, corpo, prole, gestacao,
+     file = 'C:/Users/67147020278/Documents/Introducao_Analise_Dados_R/data/dados_modulo_1_aula_3.rda')
